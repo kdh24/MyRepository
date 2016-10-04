51644c1dfc122ab2d9b0d73faf5f67ef35cf12f7
kdh24 <ehdgus24@nate.com>
2016-10-04 19:00:39 +0900

    10.04 class commit

--- HomeSpringProgramming/src/main/java/com/mycompany/home/exam06/Exam06Controller.java
+++ HomeSpringProgramming/src/main/java/com/mycompany/home/exam06/Exam06Controller.java
@@ -1,10 +1,18 @@
 package com.mycompany.home.exam06;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -44,5 +52,78 @@ public class Exam06Controller {
 		return "exam06/index";
 	}
 	
+	@RequestMapping("/method4CreateCookie")
+	public String method4CreateCookie(HttpServletResponse response) throws UnsupportedEncodingException{
+		Cookie cookie1 = new Cookie("mid", "fall");
+		String name = "홍길동";
+		name = URLEncoder.encode(name, "UTF-8");
+		Cookie cookie2 = new Cookie("mname", name);
+		cookie2.setMaxAge(60);
+		
+		response.addCookie(cookie1);
+		response.addCookie(cookie2);
+		
+		return "exam06/index";
+	}
 	
+	@RequestMapping("/method4ReceiveCookieHow1")
+	public String method4ReceiveCookieHow1(HttpServletRequest request) throws UnsupportedEncodingException{
+		//쿠키 받기 - 요청 헤더의 쿠키 정보를 읽기
+		Cookie[] cookies = request.getCookies();
+
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				String name = cookie.getName();
+				String value = null;
+				if (cookie.getName().equals("mname")) {
+					value = URLDecoder.decode(cookie.getValue(), "UTF-8");
+				} else {
+					value = cookie.getValue();
+				}
+
+				System.out.println(name + ":" + value);
+			}
+		}
+
+		return "exam06/index";
+	}
+	
+	@RequestMapping("/method4ReceiveCookieHow2")
+	public String method4ReceiveCookieHow2(@CookieValue(defaultValue="") String mid, @CookieValue(defaultValue="") String mname){
+		System.out.println("mid: " + mid);
+		System.out.println("mname: " + mname);
+		logger.info("how2 요청 처리");
+		return "exam06/index";
+	}
+
+	@RequestMapping("/method4DeleteCookie")
+	public String method4DeleteCookie(HttpServletResponse response){
+		Cookie cookie1 = new Cookie("mid", "");
+		cookie1.setMaxAge(0);
+		
+		Cookie cookie2 = new Cookie("mname", "");
+		cookie1.setMaxAge(0);
+		
+		response.addCookie(cookie1);
+		response.addCookie(cookie2);
+		
+		return "exam06/index";
+	}
+	
+	@RequestMapping("/method5SetObject")
+	public String method5SetObject(HttpSession session){
+		Member member = new Member("fall", "한가을");
+		
+		session.setAttribute("member", member);
+		
+		return "exam06/index";
+	}
+	
+	@RequestMapping("/method5GetObject")
+	public String method5GetObject(HttpSession session){
+		Member member = (Member) session.getAttribute("member");
+		
+		
+		return "exam06/index";
+	}
 }
