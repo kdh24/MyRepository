1df46860b90c323c643b66240eb9783b01be77c5
kdh24 <ehdgus24@nate.com>
2016-11-08 19:05:34 +0900

    안드로이드 11.08

--- AndroidProgramming/Exam01_Activity_LifeCycle/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ AndroidProgramming/Exam01_Activity_LifeCycle/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.administrator.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("mylog", "onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("mylog", "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("mylog", "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("mylog", "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("mylog", "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("mylog", "onDestroy()");
+    }
+
+    public void onClickBtnSecond(View view){
+        Log.i("mylog", "onClickBtnSecond()");
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickBtnThird(View view){
+        Log.i("mylog", "onClickBtnThird()");
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
     }
 }
