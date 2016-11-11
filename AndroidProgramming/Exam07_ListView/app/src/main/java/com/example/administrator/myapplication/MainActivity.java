9f4e5476217a710d7acb6421917e08093672623b
kdh24 <ehdgus24@nate.com>
2016-11-11 20:35:35 +0900

    11.11 안드로이드 commit

---
+++ AndroidProgramming/Exam07_ListView/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView lightList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lightList = (ListView)findViewById(R.id.lightView);
+
+        fillItems();
+    }
+
+    public void fillItems() {
+        List<Light> list = new ArrayList<>();
+        list.add(new Light(R.drawable.light1, "인테리어 조명1", "거실등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light(R.drawable.light2, "인테리어 조명2", "욕실등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light(R.drawable.light3, "인테리어 조명3", "자녀방등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light(R.drawable.light4, "인테리어 조명4", "현관등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light(R.drawable.light5, "인테리어 조명5", "안방등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+
+        list.add(new Light(R.drawable.light1, "인테리어 조명1", "거실등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light(R.drawable.light2, "인테리어 조명2", "욕실등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light(R.drawable.light3, "인테리어 조명3", "자녀방등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light(R.drawable.light4, "인테리어 조명4", "현관등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light(R.drawable.light5, "인테리어 조명5", "안방등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+
+        LightAdapter lightAdapter = new LightAdapter(this);
+        lightAdapter.setList(list);
+
+        lightList.setAdapter(lightAdapter);
+    }
+}
